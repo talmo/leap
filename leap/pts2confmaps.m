864c0cfb756a28e4f889843df3a8729f8f551710
wingillis <win.gillis@gmail.com>
2018-06-11 11:22:10 -0400

    a change from the master branch added here

--- leap/pts2confmaps.m
+++ leap/pts2confmaps.m
@@ -2,20 +2,21 @@ function confmaps = pts2confmaps(pts, sz, sigma, normalize)
 %PTS2CONFMAPS Generate confidence maps centered at specified points.
 % Usage:
 %   confmaps = pts2confmaps(pts, sz, sigma)
-% 
+%
 % Args:
 %   pts: N x 2
 %   sz: [rows cols]
 %   sigma: filter size (default: 5)
 %   normalize: outputs maps in [0, 1] rather than PDF (default: true)
-% 
+%
 % See also: label_joints
 
 if nargin < 3 || isempty(sigma); sigma = 5; end
 if nargin < 4 || isempty(normalize); normalize = true; end
 
 confmaps = NaN(sz(1), sz(2), size(pts,1));
-[XX,YY] = meshgrid(1:sz(1),1:sz(2));
+xv = 1:sz(2); yv = 1:sz(1);
+[XX,YY] = meshgrid(xv,yv);
 
 for i = 1:size(pts,1)
     pt = pts(i,:);
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(
+  name='LEAP',
+  version='0.0.1'
+)
